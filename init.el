3830191fd00b0e27a4ab65ed2f1f46c6d8e87dcb
nucklass <60252281+nucklass@users.noreply.github.com>
2021-10-23 14:53:23 -0700

    add doom modeline

--- init.el
+++ init.el
@@ -12,9 +12,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "c1284dd4c650d6d74cfaf0106b8ae42270cab6c58f78efc5b7c825b6a4580417" "0feb7052df6cfc1733c1087d3876c26c66410e5f1337b039be44cb406b6187c6" "7922b14d8971cce37ddb5e487dbc18da5444c47f766178e5a4e72f90437c0711" "27a1dd6378f3782a593cc83e108a35c2b93e5ecc3bd9057313e1d88462701fcd" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "97fbd952a3b01fbace2aa49b3b07692cacc3009883c7219b86e41669c2b65683" "f703efe04a108fcd4ad104e045b391c706035bce0314a30d72fbf0840b355c2c" "23b564cfb74d784c73167d7de1b9a067bcca00719f81e46d09ee71a12ef7ee82" "8f0a782ba26728fa692d35e82367235ec607d0c836e06bc39eb750ecc8e08258" default))
- '(nyan-mode t)
  '(package-selected-packages
-   '(w3m company dashboard multiple-cursors theme-magic spotify dockerfile-mode arduino-mode forth-mode rainbow-delimiters swiper ob-rust drawille csv-mode bluetooth gruvbox-theme dracula-theme scala-mode yaml-mode yaml fsharp-mode 2048-game cobol-mode dark-souls flappymacs hacker-typer isend-mode kaomoji lolcat mbo70s-theme mines moe-theme nyan-mode pacmacs poly-org rainbow-mode ranger rubik roguel-ike rustic rust-mode scad-preview scad-mode sexy-monochrome-theme spacemacs-theme sqlite3 vscode-dark-plus-theme vscode-icon python-mode powerline ob-julia-vterm material-theme julia-mode ess))
+   '(doom-modeline all-the-icons w3m company dashboard multiple-cursors theme-magic spotify dockerfile-mode arduino-mode forth-mode rainbow-delimiters swiper ob-rust drawille csv-mode bluetooth gruvbox-theme dracula-theme scala-mode yaml-mode yaml fsharp-mode 2048-game cobol-mode dark-souls flappymacs hacker-typer isend-mode kaomoji lolcat mbo70s-theme mines moe-theme nyan-mode pacmacs poly-org rainbow-mode ranger rubik roguel-ike rustic rust-mode scad-preview scad-mode sexy-monochrome-theme spacemacs-theme sqlite3 vscode-dark-plus-theme vscode-icon python-mode powerline ob-julia-vterm material-theme julia-mode ess))
  '(tool-bar-mode nil)
  '(tooltip-mode nil)
  '(warning-suppress-types '((comp))))
@@ -67,11 +66,11 @@
 ;(load-theme 'mbo70s t)
 (load-theme 'dracula t)
 
-;load powerline
-(require 'powerline)
-(powerline-default-theme)
-
- 
+;load custom modeline
+;(require 'powerline)
+;(powerline-default-theme)
+(add-hook 'after-init-hook #'doom-modeline-mode)
+(add-hook 'after-init-hook #'nyan-mode)
 ;change yes/no to y/nx
 (fset 'yes-or-no-p 'y-or-n-p)
 
